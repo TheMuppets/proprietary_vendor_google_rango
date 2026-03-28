4c57d23871f6e196d9d0fa221e8d9bd59459e9fe
Michael Bestas <mkbestas@lineageos.org>
2026-03-28 04:03:44 +0200

    rango: Import from BP4A.260205.001
    Change-Id: Iac930cd43852fb78d5d07a5ce12dbe2c21bd601b

---
+++ Android.mk
@@ -0,0 +1,33 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),rango)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,009cc065e8e4c98a6c7efe5d3559e54e26f95723)
+$(call add-radio-file-sha1-checked,radio/bl31.img,635ea2662576f7d419632d15d9a9beb30ad005aa)
+$(call add-radio-file-sha1-checked,radio/cap.img,dda5953d11c417ed639de3dbf5371678635fac8e)
+$(call add-radio-file-sha1-checked,radio/cpm.img,3e0ca79355b884e3a28ffb49c8f889d12cf68aa8)
+$(call add-radio-file-sha1-checked,radio/dbc.img,c649ad633139c79de746b1f8a58f0867be4582db)
+$(call add-radio-file-sha1-checked,radio/dbl.img,193c6af72620a9eaa171b25a1fbbf96ee75107c3)
+$(call add-radio-file-sha1-checked,radio/dram_init_0.img,d464109352bbb8d804a4a5eb5de64a1a9d1784de)
+$(call add-radio-file-sha1-checked,radio/dram_init_1.img,ba016a6d1b5579f6aa7412b5f0327b92857e268c)
+$(call add-radio-file-sha1-checked,radio/dram_init_2.img,e50a74e17ff9056d688ee8413d7692afcca2a9d1)
+$(call add-radio-file-sha1-checked,radio/dram_init_3.img,0c305eff3bb79212a52dff6a3e82ca2b615e29cf)
+$(call add-radio-file-sha1-checked,radio/dram_init_4.img,c7a5c635525a3ee1f2b4340b4bc945ed25a1ad73)
+$(call add-radio-file-sha1-checked,radio/dram_init_5.img,62d9622d5b1ba7671647bc833ba4dcc5e6c12815)
+$(call add-radio-file-sha1-checked,radio/dram_init_6.img,4e93cdcfcadc953c4f59fdcdf6ed9b691c31f020)
+$(call add-radio-file-sha1-checked,radio/dram_init_7.img,fc874fa86a6f8f7cee156f050d3d3cf7f3ff2e9e)
+$(call add-radio-file-sha1-checked,radio/dram_init_8.img,a33b792e46a7790ca7a861ec98e132d513f7d3f0)
+$(call add-radio-file-sha1-checked,radio/dram_init_9.img,8e8d2966a3f5489692c31be2d47939759fdd522b)
+$(call add-radio-file-sha1-checked,radio/dram_phy.img,f177bc72a9c46ea0eea29e005394941b881cc4b7)
+$(call add-radio-file-sha1-checked,radio/gc.img,6df011b390ef339f0dea994c9ecc07ec85512b7c)
+$(call add-radio-file-sha1-checked,radio/gdmc.img,b4aea0252f4476138ff958b8c8a87e27a6a66fd3)
+$(call add-radio-file-sha1-checked,radio/gsa_bl1.img,c03293e07f35640d858d4669cf7f2138c0713d4e)
+$(call add-radio-file-sha1-checked,radio/gsa_fw.img,c7078fea8c5586eede79cb4de9777c3e1b2c7489)
+$(call add-radio-file-sha1-checked,radio/modem.img,cde58870c004560731f7efea167ffc1e2ae17b55)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,5adc00e520453590e519acc80fd2d031bed7fd87)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,28 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bl31 \
+    cap \
+    cpm \
+    dbc \
+    dbl \
+    dram_init_0 \
+    dram_init_1 \
+    dram_init_2 \
+    dram_init_3 \
+    dram_init_4 \
+    dram_init_5 \
+    dram_init_6 \
+    dram_init_7 \
+    dram_init_8 \
+    dram_init_9 \
+    dram_phy \
+    gc \
+    gdmc \
+    gsa_bl1 \
+    gsa_fw \
+    modem \
+    tzsw
